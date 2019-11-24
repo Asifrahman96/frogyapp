8324785398df726d0b4a4540069a706314d47641
Server <server@Server.local>
2019-11-24 15:59:47 +0300

    public-read changes done

--- frogyapp/settings.py
+++ frogyapp/settings.py
@@ -134,6 +134,7 @@ AWS_S3_ENDPOINT_URL = 'https://frogyapp-storage.fra1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'storage'
 
@@ -144,6 +145,5 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
