4f98bd536936ac1d167bc3a50fd71f36bbc51914
Server <server@Server.local>
2019-11-24 14:37:45 +0300

    added / to url for spaces

--- frogyapp/settings.py
+++ frogyapp/settings.py
@@ -137,11 +137,11 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'storage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
 ]
 
 MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, '/media/'),
 ]
 
 MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
